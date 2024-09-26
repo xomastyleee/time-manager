bb353b25d8ab3539af0757e2330b0b4ae45afe12
xomastyleee <vlad.khomenko@dbbsoftware.com>
2024-09-26 23:49:51 +0300

    fix theming

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
+import { PaperProvider } from 'react-native-paper'
+import { useDefinedTheme } from '@common/hooks'
 import { AppNavigator } from './navigation/app-navigator'
 
-export const App = () => (
-  <SafeAreaProvider>
-    <AppNavigator />
-  </SafeAreaProvider>
-)
+export const App = () => {
+  const paperTheme = useDefinedTheme()
+
+  return (
+    <PaperProvider theme={paperTheme}>
+      <SafeAreaProvider>
+        <AppNavigator />
+      </SafeAreaProvider>
+    </PaperProvider>
+  )
+}
