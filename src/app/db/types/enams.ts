7268d089e60ec499abcf2e0b4b7d66f47efeabb8
Marchenko Ilya <marchenko.ilya99@gmail.com>
2024-12-08 03:25:14 +0200

    Added type enums for entity

---
+++ src/app/db/types/enams.ts
@@ -0,0 +1,34 @@
+export enum Priority {
+    A = 'A',
+    B = 'B',
+    C = 'C',
+    D = 'D',
+    F = 'F'
+}
+export enum TaskType {
+    Work = 'Work',
+    Rest = 'Rest',
+    SelfDevelopment = 'Self-Development',
+}
+export enum TaskStatus {
+    Active = 'Active',
+    Completed = 'Completed',
+    Pending = 'Pending',
+}
+export enum NotificationType {
+    Motivational = 'Motivational',
+    Reminder = 'Reminder',
+}
+export enum DayWeek {
+    Monday = 'Monday',
+    Tuesday = 'Tuesday',
+    Wednesday = 'Wednesday',
+    Thursday = 'Thursday',
+    Friday = 'Friday',
+    Saturday = 'Saturday',
+    Sunday = 'Sunday'
+}
+export enum UserStatus {
+    Active = 'Active',
+    Deactivated = 'Deactivated',
+}
