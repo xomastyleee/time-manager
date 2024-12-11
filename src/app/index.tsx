795847b1f611c21be7e60adf53460ba3806e73aa
Vlad Khomenko <144204640+xomastyleee@users.noreply.github.com>
2024-12-11 12:08:00 +0200

    rn - 0.76.1, new arch (#15)

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -6,6 +6,7 @@ import { DBProvider } from '@db/core'
 import { AppNavigator } from './navigation/app-navigator'
 
 import 'reflect-metadata'
+import 'react-native-gesture-handler'
 
 export const App = () => {
   const paperTheme = useDefinedTheme()
