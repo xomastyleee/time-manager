1e552e358d55a732107c590cd7fce8b815ec72b4
Marchenko Ilya <marchenko.ilya99@gmail.com>
2024-12-16 20:02:31 +0200

    correct

--- src/modules/common/db/types/interfaces.ts
+++ src/modules/common/db/types/interfaces.ts
@@ -1,7 +1,7 @@
 import { UserStatus } from '@common/db/types/enams'
 
 export interface IUserCreateUpdateParams {
+  status: UserStatus
   username?: string
   preferences?: string
-  status: UserStatus
 }
