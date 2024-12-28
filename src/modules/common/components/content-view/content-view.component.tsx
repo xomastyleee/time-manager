dc875bf87e4195d7e13e7dce3d57a08dea7ed057
xomastyleee <vlad.khomenko@dbbsoftware.com>
2024-12-28 13:17:26 +0200

    provide generic content-view component

---
+++ src/modules/common/components/content-view/content-view.component.tsx
@@ -0,0 +1,27 @@
+import React, { FC } from 'react'
+import { View, ViewProps } from 'react-native'
+import Animated, { useAnimatedStyle, withTiming } from 'react-native-reanimated'
+import { useCalculatedScreenGap } from '@modules/common/hooks'
+
+interface ContentViewProps extends ViewProps, React.PropsWithChildren<object> {
+  animationDuration?: number
+  isGapDisabled?: boolean
+}
+
+export const ContentView: FC<ContentViewProps> = (props) => {
+  const { viewRef, gap, handleLayout } = useCalculatedScreenGap()
+
+  const animatedStyle = useAnimatedStyle(
+    () => ({
+      height: withTiming(gap, { duration: props.animationDuration ?? 300 })
+    }),
+    [gap, props.animationDuration]
+  )
+
+  return (
+    <View ref={viewRef} {...props} onTouchStart={handleLayout}>
+      {props.children}
+      {!props.isGapDisabled && <Animated.View style={animatedStyle} />}
+    </View>
+  )
+}
