337dab59c8e2fd1ebfc359949b0c3c4b36e65afc
Marchenko Ilya <marchenko.ilya99@gmail.com>
2025-01-07 22:57:08 +0200

    correct FallbackScreen

--- src/modules/common/components/fallback-screen/fallback-screen.component.tsx
+++ src/modules/common/components/fallback-screen/fallback-screen.component.tsx
@@ -1,5 +1,7 @@
 import React, { FC, ReactNode } from 'react'
 import { useAppTheme } from '@common/hooks'
+import { Text } from 'react-native-paper'
+import { Animated, Easing, View } from 'react-native'
 
 import { stylesWithTheme } from './fallback-screen.styles'
 import { MainBackgroundView } from '../main-background-view'
@@ -13,13 +15,39 @@ interface FallbackScreenProps {
 export const FallbackScreen: FC<FallbackScreenProps> = ({ error, isLoading, children }) => {
   const theme = useAppTheme()
   const styles = stylesWithTheme(theme)
+  const spinValue = new Animated.Value(0)
+  Animated.loop(
+    Animated.timing(spinValue, {
+      toValue: 1,
+      duration: 2000,
+      easing: Easing.linear,
+      useNativeDriver: true
+    })
+  ).start()
+
+  const spin = spinValue.interpolate({
+    inputRange: [0, 1],
+    outputRange: ['0deg', '360deg']
+  })
 
   if (error) {
-    return <MainBackgroundView />
+    return (
+      <MainBackgroundView>
+        <Text style={styles.errorTitle}> Ooops... Something went wrong </Text>
+        <Text style={styles.errorMessage}> There&apos;s been an error. Try again later. </Text>
+      </MainBackgroundView>
+    )
   }
 
   if (isLoading) {
-    return <MainBackgroundView />
+    return (
+      <MainBackgroundView>
+        <View style={styles.loadingContainer}>
+          <Animated.View style={[styles.loader, { transform: [{ rotate: spin }] }]} />
+          <Text style={styles.loadingMessage}> Loading... </Text>
+        </View>
+      </MainBackgroundView>
+    )
   }
 
   return <>{children}</>
