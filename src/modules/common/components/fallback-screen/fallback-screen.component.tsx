8f09773fab9e97ce827beb0c91d46fcfbf57f419
xomastyleee <vlad.khomenko@dbbsoftware.com>
2025-01-31 20:34:15 +0200

    fixes

--- src/modules/common/components/fallback-screen/fallback-screen.component.tsx
+++ src/modules/common/components/fallback-screen/fallback-screen.component.tsx
@@ -1,4 +1,4 @@
-import { FC, ReactNode } from 'react'
+import React, { FC, PropsWithChildren } from 'react'
 import { useAppTheme } from '@common/hooks'
 import { Text } from 'react-native-paper'
 import { View } from 'react-native'
@@ -7,10 +7,9 @@ import Animated, { useSharedValue, useAnimatedStyle, withRepeat, withTiming, Eas
 import { stylesWithTheme } from './fallback-screen.styles'
 import { MainBackgroundView } from '../main-background-view'
 
-interface FallbackScreenProps {
+interface FallbackScreenProps extends PropsWithChildren {
   isError?: boolean
   isLoading?: boolean
-  children?: ReactNode
 }
 
 export const FallbackScreen: FC<FallbackScreenProps> = ({ isError, isLoading, children }) => {
