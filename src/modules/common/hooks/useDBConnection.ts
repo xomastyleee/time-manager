75ad5a3a0ab2af3ed6b10e15712663af4764da0a
Marchenko Ilya <90874301+AmateurBoy@users.noreply.github.com>
2025-01-31 19:59:33 +0100

    Bug/fix init data source provider (#23)
    * fix init database.
    
    * fix init DB provider
    
    * fix commit
    
    * fix commit
    
    * db provider improvements
    
    * correct FallbackScreen
    
    * test fix
    
    * Revert "test fix"
    
    This reverts commit 4509ef5d2f59bd471b8db321fa281d25dc184c66.
    
    * Reapply "test fix"
    
    This reverts commit 4300200c715f34e0d88ae9b2cc58338b14418da0.
    
    * fix commit
    
    * fix commit
    
    * fixes
    
    ---------
    
    Co-authored-by: xomastyleee <vlad.khomenko@dbbsoftware.com>

--- src/modules/common/hooks/useDBConnection.ts
+++ src/modules/common/hooks/useDBConnection.ts
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { DataSource } from 'typeorm'
 import { DayPlan, Goal, User, Notification, Task } from '@common/db/entities'
 import { logger } from '@common/utils'
@@ -13,16 +13,24 @@ export const dataSource = new DataSource({
 })
 
 export const useDBConnection = () => {
+  const [isInitialized, setIsInitialized] = useState(false)
+  const [isError, setIsError] = useState(false)
+
+  const initializeDB = async () => {
+    try {
+      logger.info('Initializing database...')
+      await dataSource.initialize()
+      logger.info('DataSource has been initialized!')
+      setIsInitialized(true)
+    } catch (err) {
+      logger.error('Error during DataSource initialization:', err)
+      setIsError(true)
+    }
+  }
+
   useEffect(() => {
-    dataSource
-      .initialize()
-      .then(() => {
-        logger.info('DataSource has been initialized!')
-      })
-      .catch((err) => {
-        logger.error('Error during DataSource initialization:', err)
-      })
+    initializeDB()
   }, [])
 
-  return dataSource
+  return { dataSource, isInitialized, isError }
 }
