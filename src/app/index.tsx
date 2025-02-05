d5dddf89037997fd3187915d02808381c9990851
Rostyslav14 <balaclubb@gmail.com>
2025-02-05 17:07:19 +0200

    removed initialize call

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,27 +1,16 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { PaperProvider } from 'react-native-paper'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 import { DBProvider } from '@common/db/core'
 import { useDefinedTheme } from '@common/hooks'
 
-import { initI18n } from '../locales/i18n'
 import { AppNavigator } from './navigation/app-navigator'
 
 import 'reflect-metadata'
 import 'react-native-gesture-handler'
 
 export const App = () => {
-  const [isI18nInitialized, setIsI18nInitialized] = useState(false)
   const paperTheme = useDefinedTheme()
-
-  useEffect(() => {
-    initI18n('en').then(() => setIsI18nInitialized(true))
-  }, [])
-
-  if (!isI18nInitialized) {
-    // TODO: Replace with loader
-    return null
-  }
 
   return (
     <PaperProvider theme={paperTheme}>
