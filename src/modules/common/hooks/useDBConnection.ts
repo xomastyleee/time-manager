6612d3e4e03973f76115ae61224ab5063cdfaa05
Vlad Khomenko <144204640+xomastyleee@users.noreply.github.com>
2025-02-07 18:52:31 +0200

    fixes (#43)

--- src/modules/common/hooks/useDBConnection.ts
+++ src/modules/common/hooks/useDBConnection.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import { DataSource } from 'typeorm'
 import { DayPlan, Goal, User, Notification, Task } from '@common/db/entities'
 import { logger } from '@common/utils'
@@ -16,20 +16,24 @@ export const useDBConnection = () => {
   const [isInitialized, setIsInitialized] = useState(false)
   const [isError, setIsError] = useState(false)
 
-  const initializeDB = async () => {
+  const initializeDB = useCallback(async () => {
     try {
-      logger.info('Initializing database...')
-      await dataSource.initialize()
+      if (!isInitialized) {
+        logger.info('Initializing database...')
+        await dataSource.initialize()
+      }
       logger.info('DataSource has been initialized!')
       setIsInitialized(true)
     } catch (err) {
       logger.error('Error during DataSource initialization:', err)
       setIsError(true)
     }
-  }
+  }, [isInitialized])
 
   useEffect(() => {
     initializeDB()
+    // We need to call this method only once
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   return { dataSource, isInitialized, isError }
