5d64b5a0be8b9685f9217d0f69b7c214225be30b
Marchenko Ilya <marchenko.ilya99@gmail.com>
2025-02-09 20:43:34 +0200

    #52: on dev mode dataSourse

--- src/modules/common/db/dataSource/index.ts
+++ src/modules/common/db/dataSource/index.ts
@@ -1,14 +1,12 @@
 import { DataSource } from 'typeorm'
 import { DayPlan, Goal, Notification, Task, User } from '@common/db/entities'
 
-import { UpdatePreferencesUserTable1707420000000 } from '../migrations'
-
 export const dataSource = new DataSource({
   type: 'react-native',
   database: 'test',
   location: 'default',
-  migrations: [UpdatePreferencesUserTable1707420000000],
+  // migrations: [UpdatePreferencesUserTable1707420000000], <- this set list migrations
   logging: ['error', 'query', 'schema'],
   entities: [User, DayPlan, Goal, Notification, Task],
-  synchronize: false
+  synchronize: true // <- dev mod [true]
 })
