29907e28e7a81df80bc26b5e466dc78fc88e69f9
Marchenko Ilya <marchenko.ilya99@gmail.com>
2025-02-09 19:40:07 +0200

    #32: refactor path migration

--- src/modules/common/db/dataSource/index.ts
+++ src/modules/common/db/dataSource/index.ts
@@ -1,7 +1,7 @@
 import { DataSource } from 'typeorm'
 import { DayPlan, Goal, Notification, Task, User } from '@common/db/entities'
 
-import { UpdatePreferencesUserTable1707420000000 } from '../../../../migrations'
+import { UpdatePreferencesUserTable1707420000000 } from '../migrations'
 
 export const dataSource = new DataSource({
   type: 'react-native',
