186c5a3e88656cd12d5aff706a0973195e859a53
Rostyslav14 <balaclubb@gmail.com>
2025-02-11 10:57:16 +0200

    init language detector

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,13 +4,17 @@ import { SafeAreaProvider } from 'react-native-safe-area-context'
 import { DBProvider } from '@common/db/core'
 import { useDefinedTheme } from '@common/hooks'
 
+import { detectAndInitAppLanguage } from '../locales/utils/languageDetector'
 import { AppNavigator } from './navigation/app-navigator'
 
 import 'reflect-metadata'
 import 'react-native-gesture-handler'
 
+detectAndInitAppLanguage()
+
 export const App = () => {
   const paperTheme = useDefinedTheme()
+
   return (
     <PaperProvider theme={paperTheme}>
       <SafeAreaProvider>
--- src/locales/index.ts
+++ src/locales/index.ts
@@ -3,13 +3,13 @@ import { initReactI18next } from 'react-i18next'
 
 import { resources } from './resources'
 
-export const initI18n = (language: string) =>
+export const initI18n = (lng?: string) =>
   i18next.use(initReactI18next).init({
-    lng: language,
+    lng,
     fallbackLng: 'en',
-    interpolation: {
-      escapeValue: false
-    },
+    supportedLngs: ['en', 'uk'],
+    load: 'languageOnly',
+    interpolation: { escapeValue: false },
     resources
   })
 
