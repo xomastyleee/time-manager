0d988123281c87205ab2678ae6d64c5a14863651
xomastyleee <vlad.khomenko@dbbsoftware.com>
2025-02-18 09:34:00 +0200

    discard redundant changes

--- src/modules/home/components/daily-item/daily-item.styles.ts
+++ src/modules/home/components/daily-item/daily-item.styles.ts
@@ -1,18 +1,39 @@
 import { StyleSheet } from 'react-native'
+import { type CreateStylesProps } from '@common/hooks'
 
-export const stylesWithTheme = () =>
+export const stylesWithTheme = ({ theme: { colors } }: CreateStylesProps) =>
   StyleSheet.create({
     animatedContainer: {
       overflow: 'hidden',
       width: '100%'
     },
+    currentDay: {
+      backgroundColor: colors.primary
+    },
     currentDayText: {
       fontWeight: 'bold'
+    },
+    main: {
+      backgroundColor: colors.onScreenContent,
+      borderRadius: 20,
+      marginVertical: 10,
+      minHeight: 100,
+      padding: 10,
+      width: '100%'
     },
     taskContainer: {
       alignItems: 'center',
       flex: 1,
       justifyContent: 'center',
+      width: '100%'
+    },
+    taskNavigationButton: {
+      alignItems: 'center',
+      backgroundColor: colors.onScreenContent,
+      borderRadius: 10,
+      height: 40,
+      justifyContent: 'center',
+      marginVertical: 5,
       width: '100%'
     },
     weekItem: {
--- src/modules/home/screens/create-ticket.screen.tsx
+++ src/modules/home/screens/create-ticket.screen.tsx
@@ -1,15 +1,19 @@
 import React from 'react'
+import { StyleSheet } from 'react-native'
 import { ContentView, ScreenView } from '@common/components'
+import { type CreateStylesProps, useStylesWithThemeAndDimensions } from '@common/hooks'
 
-export const CreateTicketScreen = () => (
-  // const { styles } = useStylesWithThemeAndDimensions(stylesWithTheme)
+export const CreateTicketScreen = () => {
+  const { styles } = useStylesWithThemeAndDimensions(stylesWithTheme)
 
-  <ScreenView>
-    <ContentView />
-  </ScreenView>
-)
+  return (
+    <ScreenView>
+      <ContentView style={styles.main} />
+    </ScreenView>
+  )
+}
 
-// const stylesWithTheme = ({ dimensions: { height } }: CreateStylesProps) =>
-//   StyleSheet.create({
-//     main: {}
-//   })
+const stylesWithTheme = (_: CreateStylesProps) =>
+  StyleSheet.create({
+    main: {}
+  })
